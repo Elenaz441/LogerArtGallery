fc40ca71d4ad017dd7251c659972784448158718
EZ <Elena.Zasypkina@urfu.me>
2023-05-16 15:53:27 +0500

    Initial commit

---
+++ src/main/java/com/example/logerartgallery/LogerArtGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.example.logerartgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogerArtGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogerArtGalleryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logerartgallery/LogerArtGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logerartgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogerArtGalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
